77083e1cbd27da9192e55293ffe9ea8db8e0400a
Cesar C <156138160+CesarCerda@users.noreply.github.com>
2024-01-18 16:28:37 -0600

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,2 +1,2 @@
 scene.setBackgroundImage(assets.image`starting scree`)
-music.play(music.stringPlayable("G B D G C G B F ", 200), music.PlaybackMode.UntilDone)
+music.play(music.stringPlayable("G B D G C G B F ", 200), music.PlaybackMode.LoopingInBackground)
