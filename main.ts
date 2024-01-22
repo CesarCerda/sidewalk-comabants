5dba60ef51015ef6d35030f9c01559a447dbc8b9
Cesar C <156138160+CesarCerda@users.noreply.github.com>
2024-01-22 00:15:05 -0600

    Starting screen

--- main.ts
+++ main.ts
@@ -1,2 +1,42 @@
+namespace SpriteKind {
+    export const start = SpriteKind.create()
+    export const animation = SpriteKind.create()
+}
+controller.player1.onButtonEvent(ControllerButton.Right, ControllerButtonEvent.Pressed, function () {
+    scene.setBackgroundColor(9)
+})
 scene.setBackgroundImage(assets.image`starting scree`)
+let start_up = sprites.create(img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    `, SpriteKind.animation)
+start_up.setPosition(13, 25)
+animation.runImageAnimation(
+start_up,
+assets.animation`intro starting`,
+500,
+false
+)
 music.play(music.stringPlayable("G B D G C G B F ", 200), music.PlaybackMode.LoopingInBackground)
+let button = sprites.create(assets.image`Start butto`, SpriteKind.start)
+button.setPosition(81, 89)
+animation.runImageAnimation(
+button,
+assets.animation`flashing`,
+500,
+true
+)
