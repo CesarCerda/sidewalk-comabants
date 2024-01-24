ba4b84ed494e077d7b6a8163ac81e1b0976d4954
Cesar C <156138160+CesarCerda@users.noreply.github.com>
2024-01-24 15:53:20 -0600

    Restore '1/24/2024, 3:44:40 PM beta jump'

--- main.ts
+++ main.ts
@@ -534,26 +534,6 @@ controller.player2.onButtonEvent(ControllerButton.A, ControllerButtonEvent.Press
         )
     }
 })
-controller.player2.onButtonEvent(ControllerButton.Down, ControllerButtonEvent.Pressed, function () {
-    projectile = sprites.createProjectileFromSprite(img`
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . 2 2 2 . . . . . 
-        . . . . . . . . 2 2 2 2 2 . . . 
-        . . . . . . . . 2 2 2 2 2 2 2 . 
-        2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
-        2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
-        2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
-        . . . . . . . . 2 2 2 2 2 2 2 . 
-        . . . . . . . . 2 2 2 2 2 . . . 
-        . . . . . . . . 2 2 2 . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        `, mp.getPlayerSprite(mp.playerSelector(mp.PlayerNumber.Two)), 50, 50)
-})
 sprites.onOverlap(SpriteKind.Player, SpriteKind.player2, function (sprite, otherSprite) {
     while (info.score() == 0) {
         sprite.setPosition(6, 120)
@@ -656,28 +636,7 @@ controller.player1.onButtonEvent(ControllerButton.B, ControllerButtonEvent.Press
         )
     }
 })
-controller.player1.onButtonEvent(ControllerButton.Down, ControllerButtonEvent.Pressed, function () {
-    projectile = sprites.createProjectileFromSprite(img`
-        . . . . . . . . . . . . . . . . 
-        . . . . . a a a a a a a . . . . 
-        . . . a a a a a a a a a a a . . 
-        . . a a a a a a a a a a a a a . 
-        . . a a a a a a a a a a a a a . 
-        . a a a a a a a a a a a a a a a 
-        . a a a a a a a a a a a a a a a 
-        . a a a a a a a a a a a a a a a 
-        . a a a a a a a a a a a a a a a 
-        . a a a a a a a a a a a a a a a 
-        . a a a a a a a a a a a a a a a 
-        . a a a a a a a a a a a a a a a 
-        . . a a a a a a a a a a a a a . 
-        . . a a a a a a a a a a a a a . 
-        . . . a a a a a a a a a a a . . 
-        . . . . . a a a a a a a . . . . 
-        `, mp.getPlayerSprite(mp.playerSelector(mp.PlayerNumber.One)), 50, 50)
-})
 let map_choice = 0
-let projectile: Sprite = null
 let character_choice = 0
 let button: Sprite = null
 let start_up: Sprite = null
