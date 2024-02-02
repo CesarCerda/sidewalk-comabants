f9f726263fe9fd07ec00d8675fadd98f082c68fa
Cesar C <156138160+CesarCerda@users.noreply.github.com>
2024-02-01 21:06:31 -0600

    Update main.blocks, main.ts, images.g.jres, images.g.ts

--- images.g.ts
+++ images.g.ts
@@ -1200,6 +1200,25 @@ ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff
 . . . . . . . . . . . . . . . . 
 . . . . . . . . . . . . . . . . 
 `;
+            case "image16":
+            case "finger":return img`
+. . . . . . . . . . . . . . . . 
+. . . . c c c . . . . . . . . . 
+. . . . c f c . . . . . . . . . 
+. . . . c f f c . . . . . . . . 
+. . . . c c f c . . . . . . . . 
+. . . . . c f c c . . . . . . . 
+. . . . . c f f c . . . . . . . 
+. . . . . c f f c . . . . . . . 
+. . . . . c f f c . . . . . . . 
+. . . . . c f f c . . . . . . . 
+. . . . . c f f c . . . . . . . 
+. . . . c c f f c . . . . . . . 
+. . . . c c f f c c . . . . . . 
+. . . . c f f f f c . . . . . . 
+. . . . c f f f f c . . . . . . 
+. . . . c c c c c c . . . . . . 
+`;
         }
         return null;
     })
